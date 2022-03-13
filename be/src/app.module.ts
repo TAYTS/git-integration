e287eebc2d6a9a0560d77faa30def8f2bfdfb585
TAYTS <taytzushieh@gmail.com>
2022-03-13 14:58:20 +0800

    Setup prisma service

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+    CommonModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- be/src/main.ts
+++ be/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './common/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const prismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
+  await app.listen(3001);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.log('Failed to start the app', error);
+});
