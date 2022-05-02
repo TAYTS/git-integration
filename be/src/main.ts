55dbd9b9664adf0328f0328f47d23b55cd13036e
TAYTS <taytzushieh@gmail.com>
2022-05-02 16:58:48 +0800

    Enable cors for BE

--- be/src/main.ts
+++ be/src/main.ts
@@ -3,12 +3,13 @@ import { AppModule } from './app.module';
 import { PrismaService } from './common/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const prismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
 
   await app.listen(3001);
 }
+
 bootstrap().catch((error) => {
   console.log('Failed to start the app', error);
 });
